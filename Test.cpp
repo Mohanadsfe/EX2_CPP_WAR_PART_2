d03c9f70e5ebcc537848584c7eda8427c083507f
Mohanad Sfe <92846018+Mohanadsfe@users.noreply.github.com>
2023-04-07 16:51:39 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -4,11 +4,12 @@
 #include "sources/game.hpp"
 #include "sources/card.hpp"
 using namespace std;
+using namespace ariel;
 
 TEST_CASE("The amount of cards before starting a game")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
+    Player p1("Alice");
+    Player p2("Bob");
 
     CHECK(p1.stacksize() == 0);
     CHECK(p2.stacksize() == 0);
@@ -18,9 +19,9 @@ TEST_CASE("The amount of cards before starting a game")
 
 TEST_CASE("The amount of cards after starting a game")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
 
     CHECK(p1.stacksize() == 26);
     CHECK(p2.stacksize() == 26);
@@ -30,9 +31,9 @@ TEST_CASE("The amount of cards after starting a game")
 
 TEST_CASE("The card scheme at the end of the game")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
     game.playAll();
     int sum = p1.stacksize() + p1.cardesTaken() + p2.stacksize() + p2.cardesTaken();
     CHECK(sum == 52);
@@ -40,9 +41,9 @@ TEST_CASE("The card scheme at the end of the game")
 
 TEST_CASE("Throwing errors from the functions")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
     for (int i = 0; i < 5; i++)
     {
         game.playTurn();
@@ -55,9 +56,9 @@ TEST_CASE("Throwing errors from the functions")
 
 TEST_CASE("Activating another turn after the game is over")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
     game.playAll();
     CHECK(p1.stacksize() == 0);
     CHECK(p2.stacksize() == 0);
@@ -66,41 +67,33 @@ TEST_CASE("Activating another turn after the game is over")
 
 TEST_CASE("One player")
 {
-    ariel::Player p1("Alice");
-    ariel::Game game(p1, p1);
+    Player p1("Alice");
+    Game game(p1, p1);
     CHECK_THROWS(game.playTurn());
 }
 
 TEST_CASE("Printing the winner")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
     game.playAll();
 
-    if (p1.cardesTaken() == p2.cardesTaken())
-    {
-        CHECK_THROWS(game.printWiner());
-    }
-    else
-    {
-        CHECK_NOTHROW(game.printWiner());
-    }
+    CHECK_NOTHROW(game.printWiner());
 }
 
 TEST_CASE("The game ends after at most 26 turns")
 {
-    ariel::Player p1("Alice");
-    ariel::Player p2("Bob");
-    ariel::Game game(p1, p2);
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
     int maxTurns = 26;
-    
-    for (int i = 0 ; i <=26 ; i++ && p1.stacksize()>=0)
+    int i = 0;
+    for ( ; i < 26 && p1.stacksize()>0; i++ )
     {
         game.playTurn();
     }
-    CHECK(maxTurns == 26); 
+    CHECK(maxTurns >= i );
     CHECK(p1.stacksize() == 0);
     CHECK(p2.stacksize() == 0);
-    
 }
